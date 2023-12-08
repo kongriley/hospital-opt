c0297df31db465e71aea3a236b3da55b470df46c
Diana Shklover <dshklo@mit.edu>
2023-12-08 16:58:58 -0500

    Slightly refined initial model and added hourly ems proportions

--- optimization_model.py
+++ optimization_model.py
@@ -23,14 +23,16 @@ def distance(hospital, location):
 hospital_data = open_csv("hospitals_with_beds.csv")
 # hospital_names = 
 ems_data = open_csv("ems_aggregate.csv")
+ems_hourly_data = open_csv("ems_proportion_by_hour.csv")
 
 CAR_SPEED = 20.0
+DAYS_IN_YEAR = 365.0
 HOSPITAL_COUNT = len(hospital_data)
 EMS_LOCATION_COUNT = len(ems_data)
-TIME_SLOTS = 10
+TIME_SLOTS = len(ems_hourly_data)
 
-MAX_DISTANCE = 15
-MIN_CANDIDATES = 3
+MAX_DISTANCE = 50
+MIN_CANDIDATES = 2
 
 # Only consider hospitals within reasonable distance
 distances = []
@@ -43,12 +45,18 @@ for ems_index, ems_location in enumerate(ems_data):
 		hospital_distances = closer_hospitals.copy()
 		closer_hospitals = [(distance, i, e_idx) for distance, i, e_idx in hospital_distances if distance < cur_distance]
 		cur_distance /= 1.4
+	print(len(hospital_distances))
 	distances += hospital_distances
 
 distances = {(h_idx, e_idx) : distance for distance, h_idx, e_idx in distances}
 
 # Assume all patients are immediately transported
-objective = [distance * hospital_data[h_idx][3] / (CAR_SPEED) for (h_idx, e_idx), distance in distances.items()] + [1.0/count for name, la, lo, count in hospital_data]
+travel_objective = []
+for (h_idx, e_idx), distance in distances.items():
+	travel_objective.append(distance * hospital_data[h_idx][3] / CAR_SPEED)
+hospital_count_objective = [1.0/count for name, la, lo, count in hospital_data]
+
+objective = travel_objective + hospital_count_objective
 
 # print(distances)
 
@@ -58,7 +66,7 @@ bounds = [(0, None)] * (len(distances) + HOSPITAL_COUNT)
 
 # Constraints on moving all patients to hospitals
 for e_idx, (la, lo, patient_count) in enumerate(ems_data):
-	b_eq.append(patient_count)
+	b_eq.append(patient_count / DAYS_IN_YEAR)
 	constraint = [int(e_idx == j) for i, j in distances] + [0] * HOSPITAL_COUNT
 	A_eq.append(constraint)
 
@@ -70,8 +78,9 @@ for h_idx, (name, la, lo, bed_count) in enumerate(hospital_data):
 
 
 result = linprog(c=objective,A_eq=A_eq,b_eq=b_eq,bounds=bounds)
+
 with open("initial_results.txt", "w") as f:
-	hospital_names = list(map(lambda arg:arg[0], hospital_data))
+	hospital_names = list(map(lambda arg:f"{arg[0]} ({arg[3]} beds)", hospital_data))
 	transport_labels = list(map(lambda arg:(hospital_names[arg[0]], ems_data[arg[1]][:2]),distances.keys()))
 	for info, variable_val in zip(transport_labels + hospital_names, result.x):
 		f.write(f"{str(info)}: {str(variable_val)}\n")
